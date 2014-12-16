4c88883eb4ffe6685739bcb9cddd161c34796887
mcinti <mariagrazia@socialcities.it>
2014-12-16 13:54:40 +0100

    geolocalization

--- js/start.js
+++ js/start.js
@@ -5,45 +5,7 @@ function partenza()
 {	
 	jQuery.support.cors = true;
 
-	$('body').append('<!-- MODAL ERRORI CONNESSIONE RETE -->'+
-		'<div id="modal_connection_error" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">'+
-			'<div class="modal-dialog modal-sm">'+
-				'<div class="modal-content">'+
-					'<div class="modal-header">'+
-						'<h4 class="modal-title">Errore di rete</h4>'+
-					'</div>'+
-					'<div class="modal-body">'+
-						'<p>Connessione dati non trovata</p>'+
-						'<p><div class="progress progress-striped active">'+
-							'<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>'+
-						'</div></p>'+
-					'</div>'+
-				'</div><!-- /.modal-content -->'+
-			'</div><!-- /.modal-dialog -->'+
-		'</div><!-- /.modal -->');
-
 	document.addEventListener('backbutton', backButtonCallback, true);
-	document.addEventListener("offline", offlineCallback, false);
-	document.addEventListener("online", onlineCallback, false);
-}
-
-
-//quando da online passi ad offline (perdi la rete) fai qualcosa
-function offlineCallback(){
-	if(window.location.href.indexOf("index") > -1 || window.location.href.indexOf("connection_error.html") > -1) {		
-	}
-	else{
-		scrollAlto();
-		$('#modal_connection_error').modal({"backdrop": "static"});
-	}
-}
-//quando da offline la rete torna, sblocca la situazione
-function onlineCallback(){
-	if(window.location.href.indexOf("index") > -1 || window.location.href.indexOf("connection_error.html") > -1) {		
-	}
-	else{
-		$('#modal_connection_error').modal('hide');
-	}
 }
 
 
@@ -76,4 +38,19 @@ function footerPop(){
 
 function scrollAlto(){
 	$('html, body').animate({scrollTop: 0 }, 'slow');
+}
+
+/************************/
+
+//Inizializza la geolocalizzazione veloce
+function localize()
+{ 
+	var wathcID = navigator.geolocation.getCurrentPosition(handle_localize, {enableHighAccuracy: false, maximumAge: 300000, timeout: 10000});
+}
+function handle_localize(position)
+{  	
+	sessionStorage.lat = position.coords.latitude;
+	sessionStorage.longi = position.coords.longitude;
+
+	alert('Ti trovi a: LATI:'+sessionStorage.lat+' LONGI:'+sessionStorage.longi);
 }
