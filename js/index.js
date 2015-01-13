af4f5a6c4078ab3ca8172e228ee857e5cb5b8d8e
mcinti <mariagrazia@socialcities.it>
2015-01-13 15:02:57 +0100

    commented index

--- js/index.js
+++ js/index.js
@@ -37,14 +37,14 @@ var app = {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    /*receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        /*var listeningElement = parentElement.querySelector('.listening');*/
+        var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    }*/
 };
