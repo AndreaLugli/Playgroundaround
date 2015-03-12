6470943b52af78b085cad3c804ce4fad2f458a8b
mcinti <mariagrazia@socialcities.it>
2015-03-12 13:04:48 +0100

    checkPermission everywhere

--- js/upload.js
+++ js/upload.js
@@ -54,6 +54,8 @@ function openPhotoUpload()
 		$('#container').append('<div id="emailFoto" style="display:none;" class="input-group input-group-lg"><span class="input-group-addon">Email*</span><input id="email" type="email" class="form-control" /></div>');
 		$('#container').append('<button id="completa" style="display:none; margin-bottom:50pt;" class="btn btn-lg btn-block btn-success" type="button" onClick="caricaParcoEsistente()" disabled="disabled"><i class="fa fa-check-circle-o"></i> Completa</button>');
 	}
+
+	checkPermission();
 }
 
 /***********************************SCATTO*/
